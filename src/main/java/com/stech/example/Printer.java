a2d47ff936e219767139ae73bb2b23f39bbba270
Sandeep Lulla <salulla88@gmail.com>
2018-01-22 09:32:50 +0000

    Added examples

--- src/main/java/com/stech/example/Printer.java
+++ src/main/java/com/stech/example/Printer.java
@@ -9,6 +9,16 @@ public class Printer {
     ReentrantLock lock = new ReentrantLock();
 
     public void nonSynchronizedPrinter(String name) {
+
+        //comment this if block for synchronized keyword example
+        if(lock.tryLock()) {
+            System.out.println("Got the lock...will process");
+        }
+        else {
+            System.out.println("was not able to get the lock. will return");
+            return;
+        }
+
         lock.lock(); //comment this for synchronized keyword example
         for(int i=0;i<250;i++) {
             System.out.println(name + " " + i);
